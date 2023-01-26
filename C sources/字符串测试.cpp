2dc6dfe23646380ba8515c065545ecb1b2d8e713
5Lin <2585115708@qq.com>
2023-01-26 21:43:06 +0800

    104/540

--- C sources/字符串测试.cpp
+++ C sources/字符串测试.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	char str[]="这是一个字符串测试";
+	char str[]="This is a test";
 	printf("sizeof str = %d\n",sizeof(str));
 	printf("strlen str = %d\n",strlen(str));
 	return 0;
