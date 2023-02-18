6b68c48836fae1f2febf1b5c34d80bbe8c9287c1
5Lin <2585115708@qq.com>
2023-02-18 18:38:57 +0800

    进度更新了

---
+++ C sources/test.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int cal(int,int);
+
+int main(){
+	cal(5,10);
+}
+
+
+int cal(int a,int b){
+		printf("此时的a为%d,b为%d",a,b);
+		printf("此时a*b的值为%d",a*b);
+	}
---
+++ C sources/递归法求任意斐波那契数列.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int calfbo(int,int,int);
+int main(){
+	int fbo;		
+	printf("请问你需要求第几位斐波那契数？\n");		//1,1,2,3,5,8,13
+	scanf("%d",&fbo);
+	if(fbo==1||fbo==2)
+	{
+		fbo=1;
+		printf("你所求的斐波那契数是：%d\n",fbo);
+	}
+	else{
+	//fbo=3》》斐波那契=2 				//1,1,2,3,5,8,13
+		fbo=calfbo(1,1,fbo-2);
+		printf("你所求的斐波那契数是：%d\n",fbo);
+	}
+}
+
+
+//calfbo-detail-function 
+int calfbo(int a,int b,int count){
+	count--;
+	return count!=0?calfbo(b,a+b,count):a+b;
+} 
